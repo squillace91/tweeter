21932f885af37a85a6bedf0530c5c43c43bab360
Joao Vitor Squillace Teixeira <squillace91@gmail.com>
2017-10-17 20:14:22 +0000

    Big changes -- added tweet box, counter, red-flag when reached limit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".new-tweet textarea").on('input', function(){
+        const counter = $(this).siblings('.counter');
+        const maxLimit = 140;
+        let textLength = this.value.length;
+
+        if(textLength > maxLimit){
+            counter.addClass("limit-text");
+        } else {
+            counter.removeClass("limit-text");
+        }
+
+        counter.text(maxLimit - textLength);
+    });
+});
