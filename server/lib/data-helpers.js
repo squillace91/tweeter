07b1358583e2f722e452bd51ccff7c631c654d80
Joao Vitor Squillace Teixeira <squillace91@gmail.com>
2017-10-19 19:36:40 +0000

    Merging feature/mongo to master

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -10,19 +10,11 @@ module.exports = function makeDataHelpers(db) {
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
       db.collection("tweets").insertOne(newTweet,callback);
-      // simulateDelay(() => {
-      //   db.tweets.push(newTweet);
-      //   callback(null, true);
-      // });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
       db.collection("tweets").find().toArray(callback);
-      // simulateDelay(() => {
-      //   const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-      //   callback(null, db.tweets.sort(sortNewestFirst));
-      // });
     }
 
   };
